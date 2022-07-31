f15743b0d74d0785dc290943ab055fa466cb002c
jihyunlee <jihyunlee.me@gmail.com>
2022-08-01 02:11:20 +0900

    feat: react-query injection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Button from "./components/Button/Button";
 
 const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
   return <button>버튼</button>;
+        <button>버튼</button>
+    </QueryClientProvider>
+  );
 };
 export default App;
