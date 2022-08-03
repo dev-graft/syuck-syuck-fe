a40278f58485e6ff5ad12d97ded538c50dee4e5a
jihyunlee <jihyunlee.me@gmail.com>
2022-08-03 22:51:20 +0900

    remove: recoile, react-query  제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RecoilRoot } from 'recoil';
 
 const App = () => {
-  const queryClient = new QueryClient();
-  return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <button>버튼</button>
-      </RecoilRoot>
-    </QueryClientProvider>
-  );
+  return <button>버튼</button>;
 };
 export default App;
