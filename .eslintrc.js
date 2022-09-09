74cbef1c2acb2472f4b14254b9ef780f5ad520d2
Jihyun Lee <jihyunlee.me@gmail.com>
2022-09-10 01:42:08 +0900

    saving files before refreshing line endings

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,8 @@ module.exports = {
     },
     ecmaVersion: 12,
     sourceType: "module",
-    project: "./tsconfig.json",
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
   },
   plugins: ["@typescript-eslint"],
   rules: {
