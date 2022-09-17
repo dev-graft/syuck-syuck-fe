1eb6d7097b1fb419dc49b8570ef8c29316c30dd1
jihyunlee <jihyunlee.me@gmail.com>
2022-09-17 18:51:31 +0900

    feat: route수정

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import Login
+
+const index = () => {
+  return (
+    <Routes>
+      <Route path="" element={</>}></Route>
+    </Routes>
+  );
+};
+
+export default index;
+
