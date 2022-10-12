21ca995f3d2d4b82cd2c56cfb0a12c45bdb48444
jihyunlee.me <jihyunlee.me@gmail.com>
2022-10-12 15:08:09 +0900

    chore: eslint routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Login from '@/features/auth/Login'
-import SignUp from '@/features/auth/SignUp'
-
+import Login from '@/features/auth/Login';
+import SignUp from '@/features/auth/SignUp';
 
 const Router = () => {
-  return(
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default Router;
 
