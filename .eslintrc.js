57d41ff0f79ba4e254c7d215f68e344060e1d8bc
jihyunlee.me <jihyunlee.me@gmail.com>
2022-10-12 15:02:41 +0900

    chore: .eslintrc.js 파일 double quote를 single qutoe로 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,73 +5,70 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ["*.js"],
+  ignorePatterns: ['*.js'],
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:import/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:import/typescript",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/typescript',
+    'prettier',
   ],
   settings: {
-    "react": {
-      "version": "detect",
+    react: {
+      version: 'detect',
     },
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
-    project: "tsconfig.json",
+    sourceType: 'module',
+    project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    indent: ["error", 2],
-    "no-empty": "warn",
-    "import/no-unresolved": "off",
-    "no-console": [
-      "warn",
-      { "allow": ["clear", "info", "error", "dir", "trace", "log"] }
-     ], 
-    "no-unused-vars": "off",
-    "linebreak-style": ["error", "unix"],
-    "react/prop-types": "off",
-    "react/display-name": "off",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": [
-      "warn",
+    indent: ['error', 2],
+    'no-empty': 'warn',
+    'import/no-unresolved': 'off',
+    'no-console': ['warn', { allow: ['clear', 'info', 'error', 'dir', 'trace', 'log'] }],
+    'no-unused-vars': 'off',
+    'linebreak-style': ['error', 'unix'],
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': [
+      'warn',
       {
-        "additionalHooks": "useRecoilCallback"
-      }
+        additionalHooks: 'useRecoilCallback',
+      },
     ],
-    "@typescript-eslint/no-unused-vars": 'off',
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/naming-convention": [
-      "error",
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
       // Allow camelCase variables (23.2), PascalCase variables (23.8), and UPPER_CASE variables (23.10)
       {
-        selector: "variable",
-        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
       },
       // Allow camelCase functions (23.2), and PascalCase functions (23.8)
       {
-        selector: "function",
-        format: ["camelCase", "PascalCase"],
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
       },
       // Airbnb recommends PascalCase for classes (23.3), and although Airbnb does not make TypeScript recommendations, we are assuming this rule would similarly apply to anything "type like", including interfaces, type aliases, and enums
       {
-        selector: "typeLike",
-        format: ["PascalCase"],
+        selector: 'typeLike',
+        format: ['PascalCase'],
       },
     ],
   },
