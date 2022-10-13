605604789af422b7ebf7a8081c526d1f6ed29e86
jihyunlee.me <jihyunlee.me@gmail.com>
2022-10-13 14:25:16 +0900

    feat: react-query-devtools 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom';
 
 import App from './App';
@@ -13,6 +14,7 @@ ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
   container
