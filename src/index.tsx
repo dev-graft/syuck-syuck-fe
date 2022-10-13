ae9dccadedd1fc727becce9dd321838d86aba9cb
jihyunlee.me <jihyunlee.me@gmail.com>
2022-10-13 10:12:22 +0900

    feat: react-query 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
 const container = document.getElementById('app');
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   container
 );
+
