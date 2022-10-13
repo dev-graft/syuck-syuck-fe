408bc21295165ed4dd3b8b07d96fb6955683296b
jihyunlee.me <jihyunlee.me@gmail.com>
2022-10-13 09:55:20 +0900

    feat: url constant 수정에 따른 변화

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,17 +1,10 @@
 interface IURL {
-    [URL: string]: string;
-  }
-const URL: IURL = {
+  [URL: string]: string;
+}
+export const URL: IURL = {
   // common
-  LOGIN: "/login", // 로그인
-  SIGNUP: "/signup", // 회원가입
-  MAIN: "/", // 메인 페이지
-  RESIDENT: "/resident", // 거주민
-  EMPLOYEE: "/employee", // 직원
-  SEAT: "/seat", // 좌석예약
-  SITE: "/site", // 좌석예약 리스트
-  SITE_CREATE: "/site/create", // 좌석예약
-  TIMETALBE: "", // 시간표
+  LOGIN: '/login', // 로그인
+  SIGNUP: '/signup', // 회원가입
+  MAIN: '/', // 메인 페이지
 };
-  
-export default URL;
+
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '@/features/auth/Login';
 import SignUp from '@/features/auth/SignUp';
 
+import { URL } from '@/constants/url';
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path={URL.LOGIN} element={<Login />} />
+        <Route path={URL.SIGNUP} element={<SignUp />} />
+        <Route path={URL.MAIN} element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
