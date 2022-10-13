7fbab8691e63a64b658e549c6b24ceb528a1ddfb
jihyunlee.me <jihyunlee.me@gmail.com>
2022-10-13 18:00:06 +0900

    feat: storybook storysource, theme추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,32 @@
-const path = require("path");
+const path = require('path');
 
 const resolvePath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          include: [path.resolve(__dirname, '../src')], // You can specify directories
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false },
+        },
+      },
+    },
+  ],
   webpackFinal: async (config) => ({
     ...config,
     resolve: {
       ...config.resolve,
       alias: {
         ...config.resolve.alias,
-        "@emotion/styled": resolvePath("node_modules/@emotion/styled"),
+        '@emotion/styled': resolvePath('node_modules/@emotion/styled'),
         /* 경로 별칭을 사용하는 경우 아래 주석을 해제하여 적절하게 사용한다 */
         // "@components": resolvePath("src/components"),
       },
---
+++ .storybook/manager.js
@@ -0,0 +1,8 @@
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+
+/** https://storybook.js.org/docs/react/configure/theming */
+
+addons.setConfig({
+  // theme: themes.dark,
+});
