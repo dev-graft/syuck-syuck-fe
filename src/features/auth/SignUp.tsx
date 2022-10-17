db03c522af78c3682c84b094d8f42f3c79d912ec
jihyunlee.me <jihyunlee.me@gmail.com>
2022-10-17 12:10:57 +0900

    feat: 회원가입 페이지에 modal 추가

--- src/features/auth/SignUp.tsx
+++ src/features/auth/SignUp.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function SignUp() {
+import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
+import Modal from '@/components/Modal/Modal';
+
+const SignUp = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const modalHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>SignUp</div>
-  )
-}
+    <>
+      <form>
+        <Input name="로그인" />
+        <Input name="" />
+        <Input name="" />
+        <Input name="" />
+        <Button onClick={modalHandler}>모달창 열기</Button>
+      </form>
+      {isOpen && (
+        <Modal>
+          <div>hello modal</div>
+        </Modal>
+      )}
+    </>
+  );
+};
 
-export default SignUp
+export default SignUp;
