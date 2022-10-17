4a690785511441fa7fc59315fd6fb22d9625d3db
jihyunlee.me <jihyunlee.me@gmail.com>
2022-10-17 12:09:23 +0900

    chore: Auth feature에서 page 폴더로 변경

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import { Auth } from './Auth.interface';
+import { Auth } from '../features/auth/Auth.interface';
 
 const Auth = () => {
   return <div>Auth</div>;
 };
 
 export default Auth;
+
